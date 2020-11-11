b4f8fdc97061996da84517c1ed4d3c161ed82996
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-11-10 20:59:26 -0500

    input provided CSS and HTML

---
+++ script.js
@@ -0,0 +1,6 @@
+$(".currentDay").text(moment)
+
+//function to save input to local storage
+//function to update time. if else statements for past/present/future
+//save to local storage localStorage.getItem
+//attach click event to save btn
